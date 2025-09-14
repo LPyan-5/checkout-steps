740e559c3c974dea9320864659e88058c02fbeab
LianaPyan <lp@micedesk.de>
2025-09-15 01:41:28 +0400

    success page

--- app/complete/page.tsx
+++ app/complete/page.tsx
@@ -1,18 +1,37 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useEffect, useMemo, useState } from 'react';
-import { fetchCartItems } from '@/lib/api';
 import { useStore } from '@/lib/store';
+import {useRouter} from "next/navigation";
 import { Button } from '@/components/ui/button';
-import { CartItem } from '@/components/Cart/CartItem';
-import { CartLoading } from '@/components/Cart/CartLoading';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardTitle } from '@/components/ui/card';
 
 const CompletePage = () => {
+  const router = useRouter();
+
+  const { resetCheckout } = useStore();
+
+  const handleBackToCart = () => {
+    router.push('/cart');
+
+    localStorage.removeItem('checkout-storage');
+
+    resetCheckout();
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <Card className="w-full max-w-md ">Success</Card>
+    <div className="min-h-screen flex items-center justify-center p-5">
+      <Card className="flex flex-col w-full max-w-md pt-10 gap-10 min-w-[560px]">
+        <CardTitle className="text-3xl font-bold text-center ">Success!</CardTitle>
+
+        <CardContent>
+          <Button
+            className="w-full bg-black text-white hover:bg-gray-800 disabled:opacity-50"
+            onClick={handleBackToCart}
+          >
+            Back to cart
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 };
